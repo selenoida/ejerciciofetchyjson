5f2d8a298adaf30c6dccfdb84f05202036a18515
selenoida <selenaeillen@hotmail.com>
2024-08-28 23:22:09 -0300

    fetch realizado usando showdata

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL)
+.then(response => response.json())
+
+.then(data => { 
+  console.log(data);
+  showData(data.students); })
+
+.catch(error => {
+console.log("Error al cargar los datos", error)
+});
